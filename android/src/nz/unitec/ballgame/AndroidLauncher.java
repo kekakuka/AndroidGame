3085014f4e6c2ff9a7a799186a4e15325d08c4f7
qishichang <qishichang@live.com>
2018-11-04 16:07:10 +1300

    init

---
+++ android/src/nz/unitec/ballgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package nz.unitec.ballgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import nz.unitec.ballgame.BallGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BallGame(), config);
+	}
+}
---
+++ desktop/src/nz/unitec/ballgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nz.unitec.ballgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nz.unitec.ballgame.BallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BallGame(), config);
+	}
+}
